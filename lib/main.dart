132fd67e60ee4f430d6f8f7386b120aed93928bb
omecool107 <omecool106@gmail.com>
2021-05-02 16:03:52 -0400

    lists

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
+    const questions = [
       {
         'questionText': 'What\'s your favourite colour?',
         'answers': ['Black', 'Blue', 'Red', 'Green']
