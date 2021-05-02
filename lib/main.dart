f613fb22702587f78dd26ca097dfe76e3125cd6c
omecool107 <omecool106@gmail.com>
2021-05-02 06:32:15 -0400

    quiz with question widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
-void main(){
- runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-      return MaterialApp(home: Text('Hi I`m Paul'),);
-   }
+class _MyAppState extends State<MyApp> {  
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+     questionIndex++; 
+    });
+    print(questionIndex);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+   var questions = [
+     'What\'s your favourite colour?',
+     'What\'s your favourite animal?'
+     ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(
+              questions[questionIndex]
+              ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+//        ElevatedButton()
+          ],
+        ),
+      ),
+    );
+  }
 }
