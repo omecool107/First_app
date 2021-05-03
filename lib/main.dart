47bd68236bc6f2971c16061896a384afaafbe0ce
omecool107 <omecool106@gmail.com>
2021-05-03 16:14:37 -0400

    Transaction Class created and list in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/Transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -14,14 +15,46 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions= [
+    Transaction(
+      id: 'T1', 
+      title: 'New Jacket',
+      amount: 42.69,
+      date: DateTime.now()
+      ),
+    Transaction(
+      id: 'T2', 
+      title: 'New Boots',
+      amount: 77.49,
+      date: DateTime.now()
+      ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('New Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground'),
+      body: Column(
+        
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Container(
+ //             width: double.infinity,
+ //             alignment: Alignment.center,
+              child: Text('Chart')
+              ),
+            elevation: 5,
+          ),
+          Card(
+            color: Colors.pink,
+            child: Text('List'),
+          ),
+        ],
       ),
     );
   }
